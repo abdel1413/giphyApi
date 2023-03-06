9646d5b019a1b2752ea0506f12eb1535d3422261
Your Name <you@example.com>
2023-03-06 16:55:17 -0500

    added fetch api fucnction

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 let input = document.querySelector("input");
-console.log(input);
+let section = document.querySelector(".giphy-container");
+// console.log(section);
+
+let Apikey = "zqcngqfkfg5F7HBIlf5XqPKr3E2e8inm";
 function getApi() {
-  let api = fetch("api.giphy.com/v1/gifs");
-  console.log(api);
+  input.addEventListener("keypress", (event) => {
+    console.log("e", event);
+    console.log(input);
+    let userInput = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=${Apikey}`;
+    console.log(userInput);
+    fetch(userInput)
+      .then((response) => response.json())
+      .then((datacontent) => {
+        let data = datacontent.data;
+        data.map((element) => {
+          // console.log(element.images.downsized.url);
+          let div = document.createElement("div");
+          div.className = "datacontent";
+          let img = document.createElement("img");
+          img.className = "imageBox";
+          img.src = element.images.downsized.url;
+          div.appendChild(img);
+          section.appendChild(div);
+        });
+      })
+      .catch((e) => console.error(e));
+  });
+  let url = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=${Apikey}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((content) => {
+      let data = content.data;
+      data.map((element) => {
+        // console.log(element.images.downsized.url);
+        let div = document.createElement("div");
+        div.className = "datacontent";
+        let img = document.createElement("img");
+        img.className = "imageBox";
+        img.src = element.images.downsized.url;
+        div.appendChild(img);
+        section.appendChild(div);
+      });
+    })
+    .catch((error) => console.log(error));
 }
 getApi();

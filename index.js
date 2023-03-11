3e50939eb2db8687e60efd6add797be57aabad40
Your Name <you@example.com>
2023-03-11 13:04:34 -0500

    getting vidoes working

--- index.js
+++ index.js
@@ -1,82 +1,46 @@
 let input = document.querySelector("#input");
-let section = document.querySelector(".giphy-container");
-let search = document.querySelector(".search-result");
 
 let Apikey = "zqcngqfkfg5F7HBIlf5XqPKr3E2e8inm";
-// input.addEventListener("keypress", (event) => {
-//   if (event.key == "Enter") {
-//     console.log("yes enter key is entered", input.value);
-//     input = input.value;
-//     let url = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=${Apikey}`;
-//     fetch(url)
-//       .then((response) => response.json())
-//       .then((content) => {
-//         let data = content.data;
-
-//         data.map((element) => {
-//           console.log("elements", element.url);
-//           let div = document.createElement("div");
-//           div.classList.add("datacontent");
-//           let img = document.createElement("img");
-//           img.classList.add("imageBox");
-//           img.src = element.url;
-//           div.appendChild(img);
-//           section.appendChild(div);
-//         });
-//       })
-//       .catch((e) => console.log(e));
-//   }
-// });
-
-// function getApi() {
-//   let url = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=${Apikey}`;
-
-//   fetch(url)
-//     .then((response) => response.json())
-//     .then((content) => {
-//       let data = content.data;
-//       //console.log(data)
-//       data.map((element) => {
-//         // console.log(element.images.downsized.url);
-//         let div = document.createElement("div");
-//         div.className = "datacontent";
-//         let img = document.createElement("img");
-//         img.classList.add("imageBox");
-//         //  img.src = element.images.downsized.url;
-//         img.setAttribute("src", element.images.downsized.url);
-//         div.appendChild(img);
-//         section.appendChild(div);
-//       });
-//     })
-//     .catch((error) => console.log(error));
-// }
-//getApi();
+let url = `https://api.giphy.com/v1/gifs/trending?api_key=${Apikey}&limit=50`;
+let gifContent = document.querySelector(".giphy-container");
 
 function searchGifs() {
+  let div, content;
   if (!input.value) {
-    //let url = `https://api.giphy.com/v1/gifs/search?q=${inputValue}&api_key=${Apikey}`;
-    let url = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=${Apikey}`;
+    //console.log("val", val);
     fetch(url)
       .then((response) => response.json())
       .then((dataContent) => {
         let data = dataContent.data;
-        console.log("data", data);
+        // console.log("data", data[0].images.looping);
         data.map((element) => {
-          let div = document.createElement("div");
+          //console.log(element.url);
+          div = document.createElement("div");
           div.classList.add("dataContent");
-          let img = document.createElement("img");
-          img.classList.add("imageBox");
-          img.src = element.images.downsized.url;
-          div.appendChild(img);
-          section.appendChild(div);
+
+          let video = document.createElement("video");
+          video.setAttribute("src", element.images.looping.mp4);
+          video.type = "video/mp4";
+          video.controls = true;
+          video.autoplay = true;
+          //video.muted = false;
+          video.classList.add("imageBox");
+          div.appendChild(video);
+          content = gifContent.appendChild(div);
+
+          // let img = document.createElement("img");
+          // img.classList.add("imageBox");
+          // img.src = element.images.downsized.url;
+          // // console.log("img", img);
+          // div.appendChild(img);
+          // content = gifContent.appendChild(div);
         });
       })
       .catch((e) => console.log(e));
-  } else {
+  } else if (input.value) {
     let val = input.value;
-    //console.log("val", val);
-
-    url = `https://api.giphy.com/v1/gifs/search?q=${val}&api_key=${Apikey}`;
+    console.log(val);
+    url = `https://api.giphy.com/v1/gifs/search?q=${val}&api_key=${Apikey}&limit=15`;
     fetch(url)
       .then((response) => response.json())
       .then((content) => {
@@ -84,15 +48,28 @@ function searchGifs() {
         data.map((e) => {
           let div = document.createElement("div");
           div.classList.add("dataContent");
-          let img = document.createElement("img");
-          img.classList.add("imageBox");
-          img.setAttribute("src", e.images.downsized.url);
-          div.appendChild(img);
-          section.style.display = "none";
-          search.appendChild(div);
+
+          let video = document.createElement("video");
+          video.type = "video/mp4";
+          video.controls = true;
+          video.autoplay = true;
+          video.muted = false;
+          video.classList.add("imageBox");
+          video.setAttribute("src", e.images.original.mp4);
+          div.appendChild(video);
+          content = gifContent.appendChild(div);
+
+          // let img = document.createElement("img");
+          // img.classList.add("imageBox");
+          // img.setAttribute("src", e.images.downsized.url);
+          // console.log("img", img);
+          // div.appendChild(img);
+          //content = gifContent.appendChild(div);
         });
       })
       .catch((e) => console.log(e));
   }
+  return content;
 }
+
 searchGifs();

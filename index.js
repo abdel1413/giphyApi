85a092c7bf107d599aaa30423d420897477223aa
Your Name <you@example.com>
2023-03-12 12:01:13 -0400

    fixing two issues url and how to change the innerhtml to new data

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ let gifContent = document.querySelector(".giphy-container");
 function searchGifs() {
   let div, content;
   if (!input.value) {
-    //console.log("val", val);
+    console.log("noooo value!!");
     fetch(url)
       .then((response) => response.json())
       .then((dataContent) => {
         let data = dataContent.data;
         // console.log("data", data[0].images.looping);
+        gifContent.innerHTML = "";
         data.map((element) => {
-          //console.log(element.url);
           div = document.createElement("div");
           div.classList.add("dataContent");
 
@@ -27,28 +27,23 @@ function searchGifs() {
           video.classList.add("imageBox");
           div.appendChild(video);
           content = gifContent.appendChild(div);
-
-          // let img = document.createElement("img");
-          // img.classList.add("imageBox");
-          // img.src = element.images.downsized.url;
-          // // console.log("img", img);
-          // div.appendChild(img);
-          // content = gifContent.appendChild(div);
         });
       })
       .catch((e) => console.log(e));
   } else if (input.value) {
     let val = input.value;
     console.log(val);
-    url = `https://api.giphy.com/v1/gifs/search?q=${val}&api_key=${Apikey}&limit=15`;
-    fetch(url)
+
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?q=${val}&api_key=${Apikey}&limit=15`
+    )
       .then((response) => response.json())
       .then((content) => {
         let data = content.data;
+        gifContent.innerHTML = "";
         data.map((e) => {
           let div = document.createElement("div");
           div.classList.add("dataContent");
-
           let video = document.createElement("video");
           video.type = "video/mp4";
           video.controls = true;
@@ -58,17 +53,11 @@ function searchGifs() {
           video.setAttribute("src", e.images.original.mp4);
           div.appendChild(video);
           content = gifContent.appendChild(div);
-
-          // let img = document.createElement("img");
-          // img.classList.add("imageBox");
-          // img.setAttribute("src", e.images.downsized.url);
-          // console.log("img", img);
-          // div.appendChild(img);
-          //content = gifContent.appendChild(div);
         });
       })
       .catch((e) => console.log(e));
   }
+
   return content;
 }
 
